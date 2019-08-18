a7ceb416b7615a774d3700f0ed0076ef8a25630b
Anton Strogonoff <anton@strogonoff.name>
2019-08-19 02:07:45 +0800

    feat: Copy Babel polyfills for easy inclusion

--- lib/jekyll-plugin-frontend-build/run_babel.rb
+++ lib/jekyll-plugin-frontend-build/run_babel.rb
@@ -4,6 +4,9 @@ end
 
 def build_js()
 
+  # Copy Babel polyfills
+  system('cp node_modules/@babel/polyfill/dist/polyfill.min.js _site/assets/js/babel-polyfill.js')
+
   # Transpile JS assets
   system('./node_modules/.bin/babel _site/assets/js --out-dir _site/assets/js')
 
